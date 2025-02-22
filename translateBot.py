4857dc3158ae06b522c8dae6d1cef72ea7357c9b
chaosaudit <a655332@gmx.com>
2025-02-22 18:42:31 +0000

    initial emote grabbing

--- translateBot.py
+++ translateBot.py
@@ -11,68 +11,89 @@ channels = ['chaosaudit', 'nyxipuff']
 
 ACCESS_TOKEN: str = creds_dict['language_butt']['accessToken'] #  https://twitchtokengenerator.com/
 
-
-##### WORK IN PRGRESS #####
-
-
 # Change this to False to have autotranslate turned off by default when the bot is started.
+
 statusAutotranslate = True
 
+
+# Any words added to this list will be stripped from any message before language detection / translation. 
+# Can be used to ignore words that often cause a mistranslation, emote names, usernames etc...
+# This should automatically populate with all global, channel and bttv emotes when the bot is launched.
+
+word_ignore_list = []
+
+previous_message = ""
+
+# twitchio bot and command handler.
 class Bot(commands.Bot):
 
     def __init__(self):
         super().__init__(
             token=ACCESS_TOKEN,
-            prefix='?',
+            prefix='!',
             initial_channels=channels
             )
 
     async def event_ready(self):
-        print(f'Logged in as | {self.nick}')
-        print(f'User id is | {self.user_id}')
+        print(f'\nLogged in as | {self.nick}')
+        print(f'User id is | {self.user_id}\n')
 
-        channel_ids = []
+
+        # Grabing all global / channel emotes to add to ignore list.
+        global_emotes = await self.fetch_global_emotes()
+        for emote in global_emotes:
+            word_ignore_list.append(emote.name)
+        print(f'{len(global_emotes)} global emotes added to ignore list.')
+
+        fetch_global_bttv_emotes()
+
         for channel_name in channels:
             channel = await self.fetch_channel(channel_name)
-            channel_ids.append(channel.user.id)
+            fetch_channel_emotes(channel.user.id, channel_name)
+            fetch_bttv_emotes(channel_name)
+
+        print(f'{len(word_ignore_list)} total ignored words')
+        print(f'\n##### awaiting messages #####\n')
+
+        # with open('ignorelist.txt', 'w+') as outfile:
+        #     for emote in word_ignore_list:
+        #         outfile.write(f'{emote}\n')
+
+
         
-        for id in channel_ids:
-            emote_list = requests.get(f"https://twitchemotes.com/channels/{id}")
-            print(emote_list.text)
-
-
-        global_emotes = await self.fetch_global_emotes()
-        global_emotes = [x.name for x in global_emotes]
-        print(f"Added {len(global_emotes)} global emotes to ignore list")
-        
-        # channel_emotes = await channel.fetch_emotes()
-        # channel_emotes = [x.name for x in channel_emotes ]
-        # print(channel_emotes)
-
 
     async def event_message(self, message):
         global statusAutotranslate
         # Ignore our own messages
         if message.echo:
+            previous_message = message.content
             return
+        
+        # Keeping track of the message that was sent before the current one.
+        # This allows people to use "!translate" on it's own to translate the
+        # message that was sent by the previous chatter.
+        if message.content != '!translate':
+            previous_message = message.content
 
-        # Print the contents of our message to console...
         print(message.content)
 
         if statusAutotranslate:
             try:
                 async with Translator() as translator:
-                    if len(message.content.split()) > 4:
-                        detection = await translator.detect(message.content)
+                    if len(message.content.split()) > 2:
+                        cleaned_message = [word for word in str(message.content).split() if word not in word_ignore_list]
+                        print(" ".join(cleaned_message))
+                        detection = await translator.detect(" ".join(cleaned_message))
                         if detection.lang != "en" and float(detection.confidence) > 0.85:
-                            translation = await translator.translate(message.content, dest="en")
+                            translation = await translator.translate(" ".join(cleaned_message), dest="en")
                             print(f"{detection} | {translation}")
                             await message.channel.send(f'src={detection.lang} | {message.author.name}: "{translation.text}"') 
             except Exception as e:
-                await message.channel.send(e)
+                print(e)
 
         await self.handle_commands(message)
 
+    # !autotranslate command to show the status of auto function and allow it to be turned on or off by moderators.
     @commands.command()
     async def autotranslate(self, ctx: commands.Context):
         global statusAutotranslate
@@ -94,8 +115,96 @@ class Bot(commands.Bot):
                     statusAutotranslate = True
                     await ctx.send(f"autotranslate is now [INACTIVE]. '!autotranslate on' to turn on.")
                     return
+    
+
+    # !translate command to allow manual translation of comments.
+    # !translate to:<language code> <message> to translate to languages other than English
+    # e.g. "!translate to:es hello this is a test" will translate the text to Spanish
+    @commands.command(name="translate")
+    async def translate(self, ctx: commands.Context):
+        global previous_message
+        translator = Translator()
+
+        arg_tokens = previous_message.split()
+        if previous_message.split()[0] == "!translate":
+            arg_tokens = previous_message.split()[1:]
+        
+        author = ctx.author.name
+        if "to:" in arg_tokens[0]:
+            to_lang = arg_tokens[0].split(':')[1]
+            subject = " ".join(arg_tokens[1:])
+            print(f"Translating {subject} | to {to_lang} ")
+            try:
+                translated = translator.translate(subject, dest=to_lang)
+                text = translated.text
+                new_message = f'[Translated by {author} | dest={to_lang}] - "{text}"'
+                await ctx.send(new_message[:499])
+            except Exception as e:
+                if "invalid destination language" in str(e):
+                    await ctx.send(
+                        f"Unknown destination language. "
+                        f"A list of codes can be found at: https://sites.google.com/site/opti365/translate_codes")
+                    return
+
+        else:
+            subject = " ".join(arg_tokens)
+            print(f"Translating {subject} | to en ")
+            translated = translator.translate(subject, dest='en')
+            src = translated.src
+            text = translated.text
+            new_message = f'[Translated by {author} | src={src}] - "{text}"'
+            await ctx.send(new_message[:499])
 
 
+def fetch_channel_emotes(channel_id, channel_name):
+
+    global word_ignore_list
+
+    default_badge_titles = ['cheer 1', 'cheer 100', 'cheer 1000', 'cheer 5000', 'cheer 10000', 'cheer 25000', 
+                'Subscriber', '3-Month Subscriber', '6-Month Subscriber', '9-Month Subscriber', 
+                '1-Year Subscriber', '2-Year Subscriber', 'Subscriber', '3-Month Subscriber', 
+                '6-Month Subscriber', '9-Month Subscriber', '1-Year Subscriber', '2-Year Subscriber', 
+                'Subscriber', '3-Month Subscriber', '6-Month Subscriber', '9-Month Subscriber', 
+                '1-Year Subscriber', '2-Year Subscriber']
+    
+    page = requests.get(f'https://twitch-tools.rootonline.de/emotes.php?channel_id={channel_id}')
+    channel_emotes = []
+    for line in page.text.split('\n'):
+        if 'class="mt-2 text-center"' in line:
+            emote_title = line.split('">')[1].split('<')[0]
+            if emote_title not in default_badge_titles:
+                channel_emotes.append(emote_title)
+    
+    for emote in channel_emotes:
+        if emote not in word_ignore_list:
+            word_ignore_list.append(emote)
+
+    print(f'{len(channel_emotes)} emotes added to ignore list from channel: {channel_name}')
+    
+
+def fetch_global_bttv_emotes():
+
+    global word_ignore_list
+
+    # Generate list of global bttv emotes.
+    response = requests.get('https://api.betterttv.net/3/cached/emotes/global').json()
+    for emote in response:
+        if emote['code'] not in word_ignore_list:
+            word_ignore_list.append(emote['code'])
+    print(f'{len(response)} global bttv emotes added to ignore list')
+
+
+def fetch_bttv_emotes(channel_name):
+
+    global word_ignore_list
+
+    response = requests.get(f'https://twitch.center/customapi/bttvemotes?channel={channel_name}').text
+    emotes = response.split()
+    for emote in emotes:
+        if emote not in word_ignore_list:
+            word_ignore_list.append(emote)
+    print(f'{len(emotes)} bttv emotes added to ignore list from channel: {channel_name}')
+    
 
 bot = Bot()
 bot.run()
